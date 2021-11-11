f4ec819c512b9f2617bb7e52e1763a7e7571cdc1
gorantla07 <s545503@nwmissouri.edu>
2021-11-10 22:20:40 -0600

    Added pen in controller

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ app.use('/users', usersRouter);
 app.use('/pen', penRouter);
 app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter);
-app.use('/resource', resourceRouter);
+app.use('/', resourceRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
