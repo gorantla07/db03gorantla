605973903931343b5644000a2e458e67c6b95af3
gorantla07 <s545503@nwmissouri.edu>
2021-11-17 16:05:31 -0600

    added the required code

--- controllers/pen.js
+++ controllers/pen.js
@@ -27,25 +27,27 @@ exports.Pen_create_post = async function (req, res) {
     }
 };
 // Handle Pen delete form on DELETE.
-exports.pen_delete = function (req, res) {
-    res.send('NOT IMPLEMENTED: Pen delete DELETE ' + req.params.id);
-};
-// Handle Pen update form on PUT.
-exports.pen_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: Pen update PUT' + req.params.id);
+exports.pen_delete =async function (req, res) {
+    console.log("delete "  + req.params.id) 
+    try { 
+        result = await Pen.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    } 
 };
 
 // Handle Pen update form on PUT. 
 exports.pen_update_put = async function(req, res) { 
-    console.log(`update on id ${req.params.id} with body 
-${JSON.stringify(req.body)}`) 
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`) 
     try { 
         let toUpdate = await Pen.findById( req.params.id) 
         // Do updates of properties 
-        if(req.body.pName)  
-               toUpdate.pName = req.body.pName; 
-        if(req.body.pColor) toUpdate.cost = req.body.pColor; 
-        if(req.body.pPrice) toUpdate.size = req.body.pPrice; 
+        if(req.body.pName)  toUpdate.pName = req.body.pName; 
+        if(req.body.pColor) toUpdate.pColor = req.body.pColor; 
+        if(req.body.pPrice) toUpdate.pPrice = req.body.pPrice; 
         let result = await toUpdate.save(); 
         console.log("Sucess " + result) 
         res.send(result) 
@@ -92,4 +94,60 @@ exports.pen_detail = async function(req, res) {
         res.status(500) 
         res.send(`{"error": document for id ${req.params.id} not found`); 
     } 
+};
+
+exports.pen_view_one_Page = async function(req, res) { 
+    console.log("single view for id "  + req.query.id) 
+    try{ 
+        result = await Pen.findById( req.query.id) 
+        res.render('pendetail',  
+{ title: 'Pen Detail', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+};
+
+// Handle building the view for creating a pen. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.pen_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('pencreate', { title: 'Pen Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
 }; 
+
+// Handle building the view for updating a pen. 
+// query provides the id 
+exports.pen_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await Pen.findById(req.query.id) 
+        res.render('penupdate', { title: 'Pen Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle a delete one view with id from query 
+exports.pen_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await Pen.findById(req.query.id) 
+        res.render('pendelete', { title: 'Pen Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+ 
--- routes/pen.js
+++ routes/pen.js
@@ -12,8 +12,17 @@ router.get('/', function(req, res, next) {
   res.render('pen', { title: 'Search for best pens' });
 });
 
-module.exports = router;
-
 // GET request for one pen. 
 router.get('/pen/:id', pen_controlers.pen_detail); 
+/* GET detail pen page */ 
+router.get('/detail', pen_controlers.pen_view_one_Page); 
+/* GET create pen page */ 
+router.get('/create', pen_controlers.pen_create_Page);
+/* GET create update page */ 
+router.get('/update', pen_controlers.pen_update_Page);  
+/* GET create pen page */ 
+router.get('/delete', pen_controlers.pen_delete_Page); 
+module.exports = router;
+
+
  
