8eeb39c543ef0733e09ef0fc7c5a0a9969b112e1
gorantla07 <s545503@nwmissouri.edu>
2021-11-30 17:39:37 -0600

    added security for details and delete

--- routes/pen.js
+++ routes/pen.js
@@ -22,13 +22,13 @@ router.get('/', function(req, res, next) {
 // GET request for one pen. 
 router.get('/pen/:id', pen_controlers.pen_detail); 
 /* GET detail pen page */ 
-router.get('/detail', pen_controlers.pen_view_one_Page); 
+router.get('/detail',secured, pen_controlers.pen_view_one_Page); 
 /* GET create pen page */ 
 router.get('/create', pen_controlers.pen_create_Page);
 /* GET create update page */ 
 router.get('/update',secured, pen_controlers.pen_update_Page);  
 /* GET create pen page */ 
-router.get('/delete', pen_controlers.pen_delete_Page); 
+router.get('/delete',secured, pen_controlers.pen_delete_Page); 
 module.exports = router;
 
 
